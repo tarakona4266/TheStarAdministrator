0af66210507e0cadb7a1fa5e094f429a7dd51ef2
Noemie Desplobain <g.tarakona@gmail.com>
2025-12-05 17:43:38 +0100

    planet auto rotation

---
+++ Assets/03-Scipts/RotatePlanet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class RotatePlanet : MonoBehaviour
+{
+    [SerializeField] Transform sun;
+    Vector3 direction;
+    Quaternion rotation;
+    Canvas planetInfo;
+
+    void Start()
+    {
+        planetInfo = GetComponentInChildren<Canvas>();
+    }
+
+    void Update()
+    {
+        direction = sun.position - transform.position;
+        Debug.DrawRay(transform.position, direction, Color.yellow);
+        direction.Normalize();
+        rotation = Quaternion.LookRotation(direction * -1);
+        transform.rotation = rotation;
+        Debug.Log(rotation);
+    }
+}
