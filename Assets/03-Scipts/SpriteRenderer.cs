bb61a26dba3074fffc1d684f84890e21db6fd71c
Mael <mael.lemmy2007@gmail.com>
2025-12-08 16:34:46 +0100

    OMG ÇA MARCHE

--- Assets/03-Scipts/SpriteRenderer.cs
+++ Assets/03-Scipts/SpriteRenderer.cs
@@ -7,11 +7,11 @@ public class SpriteRenderer : MonoBehaviour
 {
     [SerializeField] private Camera Camera;
     [SerializeField] private Villager Villager;   
-    private void LateUpdate()
+    private void Update()
     {
-        Vector3 cameraPosition = Camera.transform.position;
+        //Vector3 cameraPosition = Camera.transform.position;
         //transform.LookAt(cameraPosition);
-        Quaternion rotation = Quaternion.LookRotation(new Vector3(0f, Villager.FoodPlanet.transform.position.y, Villager.FoodPlanet.transform.position.z));
-        //transform.Rotate(0f, Villager.FoodPlanet.transform.position.y, Villager.FoodPlanet.transform.position.z);
+        //transform.rotation = Quaternion.Euler(90f, (Villager.FoodPlanet.transform.position.x + Villager.FoodPlanet.transform.position.z) - (Villager.transform.position.x + Villager.transform.position.z), 0f);
+        transform.rotation = Quaternion.Euler(90f, Mathf.Atan2(Villager.FoodPlanet.transform.position.x - Villager.transform.position.x, Villager.FoodPlanet.transform.position.z - Villager.transform.position.z) * Mathf.Rad2Deg, 0f);
     }
 }
